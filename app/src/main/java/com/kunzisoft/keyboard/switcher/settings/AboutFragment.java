7996de4c486d32164425fd180944444f38cc65db
J-Jamet <jeremy.jamet@kunzisoft.com>
2025-12-09 23:49:04 +0100

    fix: Fragment flow

--- app/src/main/java/com/kunzisoft/keyboard/switcher/settings/AboutFragment.java
+++ app/src/main/java/com/kunzisoft/keyboard/switcher/settings/AboutFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 
@@ -69,5 +70,16 @@ public class AboutFragment extends Fragment {
         }
 
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        PreferenceActivity activity = ((PreferenceActivity) requireActivity());
+        ActionBar toolbar = activity.getSupportActionBar();
+        if (toolbar != null)
+            toolbar.setTitle(R.string.about_title);
+        activity.showTestZone(false);
     }
 }
--- app/src/main/java/com/kunzisoft/keyboard/switcher/settings/PreferenceFragment.java
+++ app/src/main/java/com/kunzisoft/keyboard/switcher/settings/PreferenceFragment.java
@@ -2,6 +2,7 @@ package com.kunzisoft.keyboard.switcher.settings;
 
 import android.Manifest;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -13,9 +14,14 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 import androidx.preference.SeekBarPreference;
 import androidx.preference.TwoStatePreference;
 
@@ -241,6 +247,12 @@ public class PreferenceFragment extends ChromaPreferenceFragmentCompat {
 	@Override
 	public void onResume() {
 		super.onResume();
+
+        PreferenceActivity activity = ((PreferenceActivity) requireActivity());
+        ActionBar toolbar = activity.getSupportActionBar();
+        if (toolbar != null)
+            toolbar.setTitle(R.string.app_name);
+        activity.showTestZone(true);
         // To upgrade states
         checkNotification(preferenceNotification.isChecked());
         checkOverlay(preferenceOverlay.isChecked());
